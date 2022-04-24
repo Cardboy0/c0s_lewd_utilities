cd95d17170ec49a66530445ab27856b4956f22de
Cardboy0 <61648524+Cardboy0@users.noreply.github.com>
2022-04-24 15:52:34 +0200

    some more basic stuff

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# c0s_lewd_utilities
+# C0s Lewd Utilities
 Blender add-on that provides utilites (some intended for lewd stuff, some not).
+
+# Source
+https://github.com/Cardboy0/c0s_lewd_utilities
+
+# Guide
+TODO add guide
+
+# Social Media
+Twitter: https://twitter.com/cardboy0
+DeviantArt: https://www.deviantart.com/cardboy0
+Github: https://github.com/Cardboy0
---
+++ __init__.py
@@ -0,0 +1,26 @@
+import bpy
+
+bl_info = {
+    "name": "C0s Lewd Utilities",
+    "author": "Cardboy0",
+    "version": (0, 0),  # TODO: refresh version!
+    "blender": (3, 0, 0),
+    # "location": "SpaceBar Search -> Add-on Preferences Example",
+    "description": "Utilities for lewd and non-lewd related stuff",
+    # "warning": "",
+    "doc_url": "",  # TODO add link to doc
+    # "tracker_url": "bug tracker", # TODO add bugtracker?
+    "category": 'Animation',
+    "support": 'TESTING'}
+
+
+def register():
+    pass
+
+
+def unregister():
+    pass
+
+
+if __name__ == "__main__":
+    register()
